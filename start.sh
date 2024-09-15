8b785d1a05781efb827b9a2fb15674edf9a1e288
BananasAmIRite <jasony1230@gmail.com>
2024-09-15 14:08:46 -0400

    added logging

--- start.sh
+++ start.sh
@@ -3,5 +3,8 @@
 # navigate to shell directory
 cd "$(dirname "$0")"
 
-# start both in parallel
-(cd attendance-tracker-server && npm start) & (cd attendance-tracker-client && npm start)
+# make logs directory if no exist
+mkdir -p logs
+
+# start both in parallel and log
+((cd attendance-tracker-server && npm start) 2>&1 | tee ./logs/log_server_$(date '+%Y-%m-%d-%H-%M').txt) & ((cd attendance-tracker-client && npm start) 2>&1 | tee ./logs/log_client_$(date '+%Y-%m-%d-%H-%M').txt)
